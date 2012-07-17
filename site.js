bf3dab5a05245e25249117d09688e2e039c82e8e
damon <damon@serenity>
2012-07-17 22:54:13 +1000

    encode and decode

--- site.js
+++ site.js
@@ -2,3 +2,35 @@
 exports.index = function(req, res) {
   res.render('index', { title: 'Cone of Silence' });
 };
+
+exports.encode = function(req, res) {
+  var text = req.body.secretText;
+  var key = req.body.secretKey;
+
+  var crypto = require('crypto');
+
+  var plaintext = new Buffer(text);
+  var encrypted = "";
+  var cipher = crypto.createCipher('blowfish', key);
+  encrypted += cipher.update(plaintext, 'binary', 'base64');
+  encrypted += cipher.final('base64');
+
+  res.send(encrypted);
+};
+
+exports.decode = function(req, res) {
+  var text = req.body.secretText;
+  var key = req.body.secretKey;
+
+  var crypto = require('crypto');
+
+  var decrypted = "";
+  var decipher = crypto.createDecipher('blowfish', key);
+  decrypted += decipher.update(text, 'base64', 'binary');
+  decrypted += decipher.final('binary');
+
+  var output = new Buffer(decrypted); 
+
+  res.send(decrypted);
+};
+
