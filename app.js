f6b9778c614c226b7ea7c1d5301a19d5bb976381
damon <damon@serenity>
2012-07-19 22:19:22 +1000

    add file load

--- app.js
+++ app.js
@@ -12,25 +12,5 @@ app.get('/', site.index);
 app.post('/encode', site.encode);
 app.post('/decode', site.decode);
 
-app.get('/testenc', function(req, res) {
-  var crypto = require('crypto');
-  var key = 'abc123';
-
-  var plaintext = new Buffer('abcdefghijklmnopqrstuv');
-  var encrypted = "";
-  var cipher = crypto.createCipher('blowfish', key);
-  encrypted += cipher.update(plaintext, 'binary', 'base64');
-  encrypted += cipher.final('base64');
-
-  var decrypted = "";
-  var decipher = crypto.createDecipher('blowfish', key);
-  decrypted += decipher.update(encrypted, 'base64', 'binary');
-  decrypted += decipher.final('binary');
-
-  var output = new Buffer(decrypted); 
-
-  res.send('encrypted: ' + encrypted + '  decrypted: ' + decrypted);
-});
-
 app.listen(3000);
 console.log('server started');
